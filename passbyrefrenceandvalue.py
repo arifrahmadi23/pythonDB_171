8c53820a9cde6157e048ebd59133e1dd929fdd98
arifrahmadi23 <127088748+arifrahmadi23@users.noreply.github.com>
2023-11-26 10:57:32 +0700

    Delete passbyrefrenceandvalue.py

--- passbyrefrenceandvalue.py
+++
@@ -1,8 +0,0 @@
-def change( mylist):
-    mylist.append ([1,2,3,4])
-    print ( "Value inside th function: ", mylist)
-    return
-mylist = [10,20,30]
-change (mylist)
-print ("Value outside the function: ", mylist)
-
